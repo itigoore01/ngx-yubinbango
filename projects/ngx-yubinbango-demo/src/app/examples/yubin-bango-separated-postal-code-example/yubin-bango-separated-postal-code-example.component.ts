0fdcbda941e4f9df2b06f281ef5dc42364dcd43c
shota <xyz.shotao@gmail.com>
2018-08-01 23:13:20 +0900

    docs(demo): デモページを更新しました。
    close #9

---
+++ projects/ngx-yubinbango-demo/src/app/examples/yubin-bango-separated-postal-code-example/yubin-bango-separated-postal-code-example.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-yubin-bango-separated-postal-code-example',
+  templateUrl: './yubin-bango-separated-postal-code-example.component.html',
+  styleUrls: ['./yubin-bango-separated-postal-code-example.component.scss']
+})
+export class YubinBangoSeparatedPostalCodeExampleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
