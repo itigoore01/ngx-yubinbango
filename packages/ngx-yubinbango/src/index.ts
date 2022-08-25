9ff854a1c11ffdacad2c759a1ad09a7419dca539
shota <xyz.shotao@gmail.com>
2022-08-25 16:23:16 +0900

    chore(ngx-yubinbango): add export api

--- packages/ngx-yubinbango/src/index.ts
+++ packages/ngx-yubinbango/src/index.ts
@@ -1,1 +1,6 @@
 export * from './lib/ngx-yubinbango.module';
+export * from './lib/core/postal-code-resolver.service';
+export * from './lib/core/address';
+export * from './lib/directives/provider.directive';
+export * from './lib/directives/postal-code.directive';
+export * from './lib/directives/completion.directive';
